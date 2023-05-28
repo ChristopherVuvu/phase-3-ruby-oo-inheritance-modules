db01cda3038e32dfdcf6d1e96126e2b2f242bf45
ChristopherVuvu <vuvumumo93@gmail.com>
2023-05-28 22:11:43 +0300

    done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer 
+    extend MetaDancing
+    include Dance
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+angelina = Dancer.new
+angelina.twirl
+# => "I'm twirling!"
+angelina.jump
+# => "Look how high I'm jumping!"
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+    extend MetaDancing
+    include Dance
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+buster = Kid.new
+buster.jump
+# => "Look how high I'm jumping!"
+buster.take_a_bow
+# => "Thank you, thank you. It was a pleasure to dance for you all."
